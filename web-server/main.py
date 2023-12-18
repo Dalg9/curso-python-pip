85bfd1bba4cd0e37b26b9cb78ddd7df157e5699b
Daniel Lopez <lopezdanielalberto5@gmail.com>
2023-12-18 16:33:25 -0500

    html con FastAPI

--- web-server/main.py
+++ web-server/main.py
@@ -1,5 +1,25 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
+app = FastAPI()
+
+@app.get ('/')
+def get_list():
+    return [1,2,3,]
+
+@app.get ('/contact', response_class = HTMLResponse)
+def get_list():
+    return """
+          <html>
+        <head>
+            <title>HTML con Fastapi</title>
+        </head>
+        <body>
+            <h1>El clasico Hola Mundo</h1>
+        </body>
+    </html>
+    """
 def run():
     store.get_categories()
 
